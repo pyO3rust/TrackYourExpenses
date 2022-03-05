3535c72b01facb8cebe502aedde2aee154040355
RAWWR <akshayace0302@gmail.com>
2022-03-05 23:08:48 +0530

    ExpenseItem is modified

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+//useState to actually bring out the changes in the variable
 
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
@@ -11,14 +13,26 @@ const ExpenseItem = (props) => {
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 290.67;
   //props arestorere used to pass data from component a to b(forward)
+  //useState actually returns an array thus we can do array destructuring
+  //title- current status value, setTitle- function for updating the value
+
+  const [title, setTitle] = useState(props.title);
+
+  // console.log("this will get printed");
+
+  const clickHandler = () => {
+    setTitle("updated!!");
+    // console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2 className="expense-item h2">{props.title}</h2>
+        <h2 className="expense-item">{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
