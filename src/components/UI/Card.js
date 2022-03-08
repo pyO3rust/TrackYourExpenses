31fed9b1e855ba863843d3372960d207069990bf
RAWWR <akshayace0302@gmail.com>
2022-03-08 22:07:17 +0530

    UI unmodified

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Card.css";
 
-function Card(props) {
+const Card = props => {
   // const classes = 'card ' + props.className
   // props.className = expenses or expense-Item
   // props.className = `card ${props.className}`
